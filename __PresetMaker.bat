23d0a73a66560efd92a436e1b015e59123772e16
Lasal <aristides_job@hotmail.com>
2018-02-25 17:16:12 +0100

    Bug fixes, multi audio texture

--- __PresetMaker.bat
+++ __PresetMaker.bat
@@ -4,11 +4,6 @@ timeout 1
 @echo OFF
  start "" "%~dp0vvvv_50alpha35.18_x64\vvvv.exe"  /allowmultiple  /showexceptions 0  /o ..\PresetMaker\LauchControlXL_PresetMaker.v4p
 @echo OFF
-@echo --------------MIDI IN----------------
-timeout 1
-@echo OFF
- start "" "%~dp0vvvv_50alpha35.18_x64\vvvv.exe" /allowmultiple /showexceptions 0  /shutup /o ..\PresetMaker\MIDI_IN.v4p
-@echo OFF
 @echo --------------MidiLedsOut----------------
 timeout 1
 @echo OFF
